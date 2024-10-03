736c842334788b948944bdeab4bcf4a2a2897648
Salma Ahmed <salma2001ahmednady@gmail.com>
2024-10-04 00:08:36 +0300

    adding shopping cart page (component) that route when click on image in cart icon in nav bar

--- src/app/components/shoppingcart/shoppingcart.component.ts
+++ src/app/components/shoppingcart/shoppingcart.component.ts
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './shoppingcart.component.css'
 })
 export class ShoppingcartComponent {
+  quantity: number = 0; // Default quantity
 
+  increment() {
+    this.quantity++;
+  }
+
+  decrement() {
+    if (this.quantity > 0) {
+      this.quantity--;
+    }
+  }
 }
